0310c387e3d83cd034f0d3ce9ad52cadfcc206df
Sijing Aloysius Loh <cplAloysius@users.noreply.github.com>
2023-08-07 06:09:32 +0000

    fixed leap year bug

--- test_leap.c
+++ test_leap.c
@@ -6,24 +6,30 @@
 /* return 0 (false) or 1 (true), depending on whether
  * `year` is a leap year or not.
  */
-int is_leap(long year) {
+int is_leap(long year)
+{
 
-  if (year % 4 != 0) {
+  if (year % 4 != 0)
+  {
     return 0;
   }
 
-  if (year % 100 == 0) {
-    return 1;
+  if (year % 100 == 0)
+  {
+    if (year % 400 != 0)
+      return 0;
   }
 
-  return 0;
+  return 1;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   argc--;
   argv++;
 
-  if (argc != 1) {
+  if (argc != 1)
+  {
     fprintf(stderr, "Error: expected 1 command-line argument (a YEAR), but got %d\n", argc);
     exit(1);
   }
@@ -35,19 +41,25 @@ int main(int argc, char **argv) {
   long year = strtol(argv[0], &end, 10);
   int res_errno = errno;
 
-  if (end == argv[0]) {
+  if (end == argv[0])
+  {
     fprintf(stderr, "Error: couldn't interpret '%s' as a number\n", argv[0]);
     exit(1);
-  } else if (res_errno == ERANGE) {
+  }
+  else if (res_errno == ERANGE)
+  {
     fprintf(stderr, "Error: '%s' is outside the range of numbers we can handle\n", argv[0]);
     exit(1);
-  } else {
-    if (is_leap(year)) {
+  }
+  else
+  {
+    if (is_leap(year))
+    {
       printf("%ld is a leap year\n", year);
-    } else {
+    }
+    else
+    {
       printf("%ld is not a leap year\n", year);
     }
   }
-
 }
-
